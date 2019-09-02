0fd9fef5edd8ca1d6a6a0d6f481c063a34664d6e
BayLadyCoder <rachada_32@hotmail.com>
2019-09-02 18:35:46 -0400

    Updates

---
+++ precache-manifest.a262fec3ab931a2ef8a0ffb1abd7f7a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aec1060d010bd536f4cbdde0e4da187",
+    "url": "/contact-manager/index.html"
+  },
+  {
+    "revision": "28e5c4d51e6136313dc5",
+    "url": "/contact-manager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "e7d455e9c1139cd07646",
+    "url": "/contact-manager/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "28e5c4d51e6136313dc5",
+    "url": "/contact-manager/static/js/2.d7eaecaa.chunk.js"
+  },
+  {
+    "revision": "e7d455e9c1139cd07646",
+    "url": "/contact-manager/static/js/main.5dc8845c.chunk.js"
+  },
+  {
+    "revision": "121ee6a4ab8d95abcdb4",
+    "url": "/contact-manager/static/js/runtime~main.d47d8c02.js"
+  }
+]);
